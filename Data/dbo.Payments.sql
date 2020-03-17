847c7d3b625f4d3234a363cc68e2e0feb034cacf
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-17 11:10:11 +0000

    Same

--- Data/dbo.Payments.sql
+++ Data/dbo.Payments.sql
@@ -1,13 +1,16 @@
-﻿CREATE TABLE [dbo].Payments
-(
-	[PaymentIdentifier] NVARCHAR(100) NOT NULL PRIMARY KEY,
-	[Amount] DECIMAL(19,4) NOT NULL,
-	[Currency] VARCHAR(3) NOT NULL,
-	[MaskedCardNumber] VARCHAR(16) NOT NULL,
-	[ExpiryMonthAndDate] VARCHAR(4) NOT NULL,
-	[Cvv] VARCHAR(3) NOT NULL,
-	[MerchantId] UNIQUEIDENTIFIER NOT NULL
-)
-GO
+﻿
+CREATE TABLE [dbo].[Payments] (
+    [PaymentIdentifier]  NVARCHAR (100)   NOT NULL,
+    [Amount]             DECIMAL (19, 4)  NOT NULL,
+    [Currency]           VARCHAR (3)      NOT NULL,
+    [MaskedCardNumber]   VARCHAR (16)     NOT NULL,
+    [ExpiryMonthAndDate] VARCHAR (4)      NOT NULL,
+    [Cvv]                VARCHAR (3)      NOT NULL,
+    [MerchantId]         UNIQUEIDENTIFIER NOT NULL,
+    [StatusCode]         NVARCHAR (100)   NOT NULL
+);
+
+
+
 
 -- CREATE CLUSTERED INDEX IX_Payments_PaymentIdentifier ON dbo.Payments(PaymentIdentifier)
