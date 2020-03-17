342210501765a8a7794fe6a1f3dc6d8ad1c2ff79
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-17 22:34:01 +0000

    removed unneeded comments

--- PaymentGateway/Controllers/PaymentsController.cs
+++ PaymentGateway/Controllers/PaymentsController.cs
@@ -67,9 +67,6 @@ namespace PaymentGateway.Controllers
 
         [HttpGet("{paymentIdentifier}")]
 
-      //  [Consumes(MediaTypeNames.Application.Json)]
-        //[ProducesResponseType(StatusCodes.Status201Created)]
-        //  [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<PaymentDetails>> Get(string paymentIdentifier)
         {
             var payment = await _paymentRepository.Get(paymentIdentifier);
