24dcf81f5e76970921b8f5b1c8f2b0f817536e2a
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-17 08:08:58 +0000

    ExpiryMonthAndDate field fix

--- PaymentGateway/Models/PaymentDetails.cs
+++ PaymentGateway/Models/PaymentDetails.cs
@@ -13,7 +13,7 @@ namespace PaymentGateway.Models
         public string Currency { get; set; }  //todo GBP/EUR/USD? what are the currencies supported by the bank?
 
         public string MaskedCardNumber { get; set; }  // custom type?
-        public string ExpiryMonthDate { get; set; } // custom type?
+        public string ExpiryMonthAndDate { get; set; } // custom type?
         public string Cvv { get; set; }
 
         public Guid MerchantId { get; set; }
