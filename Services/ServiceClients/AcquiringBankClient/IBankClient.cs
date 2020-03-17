267a906724a5120acacc488d66e60588df037d99
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-17 18:11:24 +0000

    Revert "Added http status code to bank client results"
    This reverts commit cdda4071ed038f6305b62f06c1c1671c88766242.

--- Services/ServiceClients/AcquiringBankClient/IBankClient.cs
+++ Services/ServiceClients/AcquiringBankClient/IBankClient.cs
@@ -1,12 +1,10 @@
 ﻿using PaymentGateway.Services.ServiceClients.AcquiringBankClient.Models;
-using System;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace PaymentGateway.Services.ServiceClients
 {
     public interface IBankClient
     {
-        Task<Tuple<BankPaymentResponse, HttpStatusCode>> ProcessPayment(BankPaymentRequest request);
+        Task<BankPaymentResponse> ProcessPayment(BankPaymentRequest request);
     }
 }
