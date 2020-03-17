1aaa9a7a6a978851c192a7eff14c29d5ae6a09b8
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-17 17:42:24 +0000

    renamed a property, added comments, improved bank simulator

--- BankSimulator/Controllers/BankPaymentsController.cs
+++ BankSimulator/Controllers/BankPaymentsController.cs
@@ -19,22 +19,26 @@ namespace BankSimulator.Controllers
             _logger = logger;
         }
 
+        // ASSUMPTIONS
+        // Acquiring bank exposes a RESTful HTTP API
+        // The payment processing endpoint
+        //  - returns 200 and payment identifier for successful requests, 
+        //  - returns 4xx/5xx, payment identifier and error code for failed requests 
         [HttpPost]
-        public async Task<BankPaymentResponse> ProcessPayment(BankPaymentRequest request)
+        public async Task<ActionResult<BankPaymentResponse>> ProcessPayment(BankPaymentRequest request)
         {
             if (request.PaymentCardNumber.Length != 16)
             {
-                return new BankPaymentResponse()
-                {
-                    PaymentIdentifier = Guid.NewGuid().ToString(),
-                    PaymentStatusCode = "Failure_IncorrectCardNumber"
-                };
+                return BadRequest(new BankPaymentResponse()
+                                {
+                                    PaymentIdentifier = Guid.NewGuid().ToString(),
+                                    PaymentErrorCode = "Failure_IncorrectCardNumber"
+                                });
             }
 
             return new BankPaymentResponse()
             {
                 PaymentIdentifier = Guid.NewGuid().ToString(),
-                PaymentStatusCode = "Success" //todo different code depending on request
             };
         }
     }
--- Services/ServiceClients/AcquiringBankClient/BankClient.cs
+++ Services/ServiceClients/AcquiringBankClient/BankClient.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 
 namespace PaymentGateway.Services.ServiceClients
 {
+    // ASSUMPTIONS
+    // Acquiring bank exposes a RESTful HTTP API
+    // The payment processing endpoint
+    //  - returns 200 and payment identifier for successful requests, 
+    //  - returns 4xx/5xx, payment identifier and error code for failed requests 
     public class BankClient : IBankClient
     {
         private readonly HttpClient _client;
@@ -21,16 +26,16 @@ namespace PaymentGateway.Services.ServiceClients
         public async Task<BankPaymentResponse> ProcessPayment(BankPaymentRequest request)
         {
             var url = _configuration["Dependencies:AcquiringBank:ProcessPaymentEndpointUrl"];
-            // todo authentication
+            // todo authentication comment
             var content = JsonConvert.SerializeObject(request);
             var httpResponse = await _client.PostAsync(url, new StringContent(content, System.Text.Encoding.Default, "application/json"));
 
-            if (!httpResponse.IsSuccessStatusCode)
-            {
-                // todo are retries allowed?
-                throw new Exception("Cannot make a payment"); // todo return error message to the client? analyse response code?
-            }
+            // todo are retries allowed?
 
+            // todo check HTTP status code and set payment status accordingly?
+
+            // todo consider 500 and non-json response
+        
             var response = JsonConvert.DeserializeObject<BankPaymentResponse>(await httpResponse.Content.ReadAsStringAsync());
             return response;
         }
