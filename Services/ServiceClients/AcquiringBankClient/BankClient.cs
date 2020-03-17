cdda4071ed038f6305b62f06c1c1671c88766242
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-17 18:06:29 +0000

    Added http status code to bank client results

--- Services/ServiceClients/AcquiringBankClient/BankClient.cs
+++ Services/ServiceClients/AcquiringBankClient/BankClient.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using PaymentGateway.Services.ServiceClients.AcquiringBankClient.Models;
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -23,21 +24,21 @@ namespace PaymentGateway.Services.ServiceClients
             _configuration = configuration;
         }
          
-        public async Task<BankPaymentResponse> ProcessPayment(BankPaymentRequest request)
+        public async Task<Tuple<BankPaymentResponse,HttpStatusCode>> ProcessPayment(BankPaymentRequest request)
         {
             var url = _configuration["Dependencies:AcquiringBank:ProcessPaymentEndpointUrl"];
+
             // todo authentication comment
             var content = JsonConvert.SerializeObject(request);
             var httpResponse = await _client.PostAsync(url, new StringContent(content, System.Text.Encoding.Default, "application/json"));
 
             // todo are retries allowed?
 
-            // todo check HTTP status code and set payment status accordingly?
-
             // todo consider 500 and non-json response
         
             var response = JsonConvert.DeserializeObject<BankPaymentResponse>(await httpResponse.Content.ReadAsStringAsync());
-            return response;
+
+            return new Tuple<BankPaymentResponse, HttpStatusCode>(response,httpResponse.StatusCode);
         }
     }
 }
--- Services/ServiceClients/AcquiringBankClient/IBankClient.cs
+++ Services/ServiceClients/AcquiringBankClient/IBankClient.cs
@@ -1,10 +1,12 @@
 ﻿using PaymentGateway.Services.ServiceClients.AcquiringBankClient.Models;
+using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace PaymentGateway.Services.ServiceClients
 {
     public interface IBankClient
     {
-        Task<BankPaymentResponse> ProcessPayment(BankPaymentRequest request);
+        Task<Tuple<BankPaymentResponse, HttpStatusCode>> ProcessPayment(BankPaymentRequest request);
     }
 }
