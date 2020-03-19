e7b482d575dc9ce1d77ae5f0d8ae7492ce242476
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-19 21:50:04 +0000

    New DB script and readme changes

--- Data/dbo.Payments.sql
+++ Data/dbo.Payments.sql
@@ -1,16 +1,23 @@
 ﻿
+-- PaymentId is GUID and is a primary key. Clustered index is on a different column (int identity) in order to avoid performance issues 
+-- https://stackoverflow.com/questions/11938044/what-are-the-best-practices-for-using-a-guid-as-a-primary-key-specifically-rega/11938495
+
 CREATE TABLE [dbo].[Payments] (
-    [PaymentIdentifier]  NVARCHAR (100)   NOT NULL,
-    [Amount]             DECIMAL (19, 4)  NOT NULL,
+    [PaymentId] UNIQUEIDENTIFIER NOT NULL,        
+    [AcquringBankPaymentId]  NVARCHAR (100) NULL,
+    [StatusCode]		 INT NOT NULL,        
+	[Amount]             DECIMAL (19, 4)  NOT NULL,
     [Currency]           VARCHAR (3)      NOT NULL,
     [MaskedCardNumber]   VARCHAR (16)     NOT NULL,
     [ExpiryMonthAndDate] VARCHAR (4)      NOT NULL,
     [Cvv]                VARCHAR (3)      NOT NULL,
     [MerchantId]         UNIQUEIDENTIFIER NOT NULL,
-    [StatusCode]         NVARCHAR (100)   NOT NULL
+	
+    [CreatedDate] DATETIME CONSTRAINT [DF_Payments_CreatedDate] DEFAULT (getutcdate()) NOT NULL,      
+    [ClusterID] INT NOT NULL IDENTITY,
+    CONSTRAINT [PK_Payments] PRIMARY KEY NonCLUSTERED ([PaymentID] ASC)
 );
+GO
 
-
-
-
--- CREATE CLUSTERED INDEX IX_Payments_PaymentIdentifier ON dbo.Payments(PaymentIdentifier)
+CREATE UNIQUE CLUSTERED INDEX [IX_Payments_ClusterID] ON [dbo].[Payments] ([ClusterID])
+GO
