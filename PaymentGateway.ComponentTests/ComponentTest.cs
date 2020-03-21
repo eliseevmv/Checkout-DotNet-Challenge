bd1cabf663a5ade3355b21aef15c906a1d899126
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-21 16:55:03 +0000

    Component test improvements

--- PaymentGateway.ComponentTests/ComponentTest.cs
+++ PaymentGateway.ComponentTests/ComponentTest.cs
@@ -1,54 +1,58 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Moq;
-using Newtonsoft.Json;
 using NUnit.Framework;
 using PaymentGateway.ComponentTests.Builders;
 using PaymentGateway.ComponentTests.Infrastructure;
 using PaymentGateway.Models;
 using PaymentGateway.Services.Entities;
 using PaymentGateway.Services.Repositories;
+using PaymentGateway.Services.ServiceClients.AcquiringBankClient;
+using PaymentGateway.Services.ServiceClients.AcquiringBankClient.Models;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PaymentGateway.ComponentTests
 {
-    public class ComponentTest //: CustomWebApplicationFactory<TestStartup>
+    public class ComponentTest 
     {
         private readonly PaymentGatewayClient client;
         private readonly Mock<IPaymentRepository> paymentRepositoryMock;
+        private readonly Mock<IBankClient> bankClientMock;
 
+        // todo ensure mocks are recreated for every test
         public ComponentTest()
         {
             var builder = new WebHostBuilder().UseStartup<TestStartup>();
-
-
-            builder.ConfigureServices(services => {
-          
-
-            });
             var testServer = new TestServer(builder);
-            this.client = new PaymentGatewayClient( testServer.CreateClient());
+            this.client = new PaymentGatewayClient(testServer.CreateClient());
 
-            var x = testServer.Services.GetService(typeof(Mock<IPaymentRepository>));
-            paymentRepositoryMock = (Mock<IPaymentRepository>)x;
-
+            paymentRepositoryMock = GetMock<IPaymentRepository>(testServer);
+            bankClientMock = GetMock<IBankClient>(testServer);
         }
 
         [Test]
-        public async Task tmp2()
+        public async Task Given_valid_request_when_ProcessPayment_and_bank_returns_200_then_returns_200()
         {
             paymentRepositoryMock.Setup(x => x.Save(It.IsAny<Payment>())).Returns(Task.CompletedTask);
             paymentRepositoryMock.Setup(x => x.Update(It.IsAny<Payment>())).Returns(Task.CompletedTask);
+            bankClientMock.Setup(x => x.ProcessPayment(It.IsAny<BankPaymentRequest>())).ReturnsAsync(new BankPaymentResponseWithStatus()
+            {
+                StatusCode = HttpStatusCode.OK,
+                ResponseBody = new BankPaymentResponse
+                {
+                    PaymentIdentifier = "someIdentifier",
+                    PaymentErrorCode = null
+                }
+            }); ;
 
             var request = PaymentRequestBuilder.BuildValidPaymentRequest();
 
             var response = await client.Post<ProcessPaymentRequest, ProcessPaymentResponse>("/payments", request);
 
-
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.ResponseBody.StatusCode, Is.EqualTo(PaymentStatusCode.Success.ToString()));
+            Assert.That(response.ResponseBody.PaymentId, Is.Not.Null.And.Not.Empty));
         }
 
 
@@ -73,5 +77,12 @@ namespace PaymentGateway.ComponentTests
 
         //    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         //}
+
+
+        private static Mock<T> GetMock<T>(TestServer testServer) where T : class
+        {
+            return (Mock<T>)testServer.Services.GetService(typeof(Mock<T>));
+        }
+
     }
 }
--- PaymentGateway.ComponentTests/TestStartup.cs
+++ PaymentGateway.ComponentTests/TestStartup.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Moq;
 using PaymentGateway.Services.Repositories;
+using PaymentGateway.Services.ServiceClients.AcquiringBankClient;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -13,22 +14,26 @@ namespace PaymentGateway.ComponentTests
     public class TestStartup : Startup
     {
         private readonly Mock<IPaymentRepository> _paymentRepository;
+        private readonly Mock<IBankClient> _bankClient;
 
         public TestStartup(IConfiguration configuration)
             : base(configuration)
         {
             this._paymentRepository = new Mock<IPaymentRepository>(MockBehavior.Strict);
+            this._bankClient = new Mock<IBankClient>(MockBehavior.Strict);
         }
 
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().AddApplicationPart(Assembly.Load(new AssemblyName("PaymentGateway")));
 
-            services.AddSingleton(this._paymentRepository);  //todo remove?
+            services.AddSingleton(this._paymentRepository);  
+            services.AddSingleton(this._bankClient);  
 
             base.ConfigureServices(services);
 
             services.AddSingleton(this._paymentRepository.Object);
+            services.AddSingleton(this._bankClient.Object);
         }
     }
 }
