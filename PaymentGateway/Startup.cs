e15e31b2820ee6c35cc0533a2381080965f8eddb
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-21 13:26:05 +0000

    component tests - v1

--- PaymentGateway/Startup.cs
+++ PaymentGateway/Startup.cs
@@ -20,7 +20,7 @@ namespace PaymentGateway
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
+        public virtual void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IConfiguration>(Configuration);
 
@@ -38,7 +38,7 @@ namespace PaymentGateway
             services.AddApplicationInsightsTelemetry();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
