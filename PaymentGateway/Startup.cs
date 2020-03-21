a43d34a63bde8ca952db8000377f5455fb67d70f
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-21 11:51:26 +0000

    remove blob

--- PaymentGateway/Startup.cs
+++ PaymentGateway/Startup.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Azure;
 using PaymentGateway.Services.Repositories;
 using AutoMapper;
-using PaymentGateway.Services.ServiceClients;
 using PaymentGateway.Services.Services;
 using PaymentGateway.Services.ServiceClients.AcquiringBankClient;
 
@@ -44,12 +36,6 @@ namespace PaymentGateway
 
             services.AddControllers();
             services.AddApplicationInsightsTelemetry();
-
-            //todo removr
-            services.AddAzureClients(builder =>
-            {
-                builder.AddBlobServiceClient(Configuration["ConnectionStrings:Storage1"]);
-            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
