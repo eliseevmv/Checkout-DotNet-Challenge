00f37b5fbba4c82914a5b483e2f7d00a1ab7636b
maxim_eliseev <maxim_eliseev@yahoo.co.uk>
2020-03-26 00:25:25 +0000

    Unit test

--- PaymentGateway.UnitTests/Services/PaymentValidationServiceTests.cs
+++ PaymentGateway.UnitTests/Services/PaymentValidationServiceTests.cs
@@ -2,12 +2,14 @@ using NUnit.Framework;
 using PaymentGateway.Services.Services;
 using PaymentGateway.UnitTests.Common;
 using System;
+using System.Linq;
 using Payment = PaymentGateway.Services.Entities.Payment;
 
 namespace PaymentGateway.UnitTests
 {
     // This is an example of a unit test
     // It tests a class in isolation. All dependencies should be replaced by test doubles, eg mocks
+    // Ideally most classes in the solution, including PaymentsController and PaymentService, should have a set of unit tests
     public class PaymentValidationServiceTests
     {
         private PaymentValidationService paymentValidationService; 
@@ -29,8 +31,21 @@ namespace PaymentGateway.UnitTests
             CollectionAssert.IsEmpty(result);
         }
 
+        [Test]
+        public void Given_payment_with_negative_amount_when_validates_should_return_error_message()
+        {
+            var paymentEntity = PaymentEntityBuilder.CreateValidPayment();
+            paymentEntity.Amount = -1;
+
+            var result = paymentValidationService.Validate(paymentEntity);
+
+            Assert.IsNotNull(result);
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(result.First(), Does.Contain("negative"));
+        }
+
         // All other validation scenarios
 
-       
+
     }
 }
